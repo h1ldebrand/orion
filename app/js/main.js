79502d6b63e01240133a81312312b73e9121255d
Vadim Semenchuk <pro100_developer@ukr.net>
2017-03-03 23:11:49 +0200

    second evening

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,11 @@
 $(document).ready(function(){
 
-	//метод назначает одинаковую высоту указанным селекторам
-	$.fn.sameHeight = function(){
-		var icount = 1;
-		this.each(function(){
-			if(icount < $(this).height())
-				icount = $(this).height();
-			else
-				icount = icount;
-		})
-		return this.height(icount);
-	}
-
+	
+	//подключаем слайдер owl-carousel
 	$(".slider").owlCarousel({
 		items: 1,
 		loop:true,
+		navText: ['', ''],
 		autoplay: true,
 		autoplayTimeout: 10000
 	})
@@ -23,16 +14,71 @@ $(document).ready(function(){
 		$(this).html("<span></span>");
 	})
 
-	$(".service-item").sameHeight();
-
-	var headerHeight = $(".header").first().height();
-
-
-	$(".slider").css("min-height", headerHeight)
 
 
 
+	$(".slider_image").each(function(){
+		var that = $(this);
+		var sliderWidth = that.closest(".slider").css("width");
+		var sliderHeight = that.closest(".slider").css("height");
+		that.css("min-width", sliderWidth);
+		that.css("min-height", sliderHeight);
+	})
+
+	
+	
+	$(".operation").each(function(e){
+
+		var that = $(this);
+		var operationTop = 0;
+
+		if(e === 0){
+			operationTop = that.offset({top: $(".working-process").offset().top +25})
+			.offset().top;
+		} else {
+			operationTop = that.offset({top: that.prev().offset().top + that.prev().outerHeight() + 25})
+			.offset().top;
+		}
+
+		if(e%2 == 0){
+
+			that.addClass("operation_right");
+			addElement("blue", operationTop);
+
+		} else {
+
+			that.addClass("operation_left");
+			addElement("red", operationTop);
+
+		}
+		
+		function addElement(color, operationTop){
+
+			var circleTop = operationTop + 25;
+			$("<div class='scale__circle'></div>")
+				.appendTo(".scale__circle-operation-wrap")
+					.addClass("scale__circle_"+color)
+						.offset({top: circleTop});
+
+		}
+		if(e == $(".operation").length - 1){
+			var lastTop = $(".operation").last().offset().top;
+			var firstTop = $(".operation").first().offset().top;
+			var lastHeight = $(".operation").last().outerHeight();
+
+			var scaleLineHeight = lastTop - firstTop + lastHeight + ($(".operation").length + 1) * 25;
+			
+			$(".scale__line").height(scaleLineHeight)
+			console.log(lastTop);
+			console.log(firstTop);
+			console.log(lastHeight);
+			console.log(scaleLineHeight);
+		}
+		
+	});
 
 
+	
+		
 
 })
